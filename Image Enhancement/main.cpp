dd8d3b3bfea476f491a9363a5f96935642450d9b
Egin <leeeezian@gmail.com>
2026-01-11 02:25:46 +0800

    main argv argc setup

--- Image Enhancement/main.cpp
+++ Image Enhancement/main.cpp
@@ -4,43 +4,73 @@
 #include <iostream>
 #include <filesystem>
 
-int main(int argc, char** argv) {
-	if (argc <= 0) {
-		std::cout << "parameter : imagePath(string), sigmaValue(float), detailAmplification(float)*, denoiseInputImage(bool)*";
-	}
+struct ProgramArguments
+{
+	std::string image_path;
+	float sigma;
+	float detailAmplification;
+	bool denoiseInputImage;
+};
 
-	//std::string image_path{ argv[0]};
-	//float sigma{ std::stof(argv[1]) };	
-	//float detailAmplification{ std::stof(argv[2]) };
-	//bool denoiseInputImage{ std::stoi(argv[3]) };
+std::optional< ProgramArguments> parseArgs(int argc, char** argv);
+
+int main(int argc, char** argv) {
+
+	auto args{ parseArgs(argc,argv) };
+	if (!args) {
+		return 1;
+	}
 
 
 	// Kernel initialisation
-	const GaussianKernel testingKernal{ 5.0f };
+	const GaussianKernel testingKernal{ args->sigma };
 
-	// Reading image
-	std::string image_path{ "C:/Users/User/Desktop/Egin/opencvTesting/lenna.png" };
-
-	std::filesystem::path pathObj(image_path);
+	std::filesystem::path pathObj(args->image_path);
 
 	// Get just the filename
 	std::string filename = pathObj.filename().string();
 
-	//cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-	//if (image.empty()) {
-	//	std::cerr << "Error: Could not load image at " << image_path << std::endl;
-	//	return -1;
-	//}
-
-	cv::Mat image{ cv::imread(image_path, cv::IMREAD_COLOR) };
+	cv::Mat image{ cv::imread(args->image_path, cv::IMREAD_COLOR) };
 	if (image.empty()) {
-		std::cerr << "Error: Could not load colored image at " << image_path << std::endl;
+		std::cerr << "Error: Could not load colored image at " << args->image_path << std::endl;
 		return -1;
 	}
 
-	
+
 	enhanceImage(image, testingKernal, filename);
 
 }
 
+std::optional< ProgramArguments> parseArgs(int argc, char** argv) {
+	if (argc < 3 || argc > 5) {
+		std::cerr << "Usage: " << argv[0]
+			<< " \n<imagePath>(string) \n<sigma>(float) \n[detailAmp](float) \n[denoise](bool)\n";
+		return std::nullopt;
+	}
+
+	ProgramArguments args{};
+
+	try {
+		args.image_path = argv[1];
+		args.sigma = std::stof(argv[2]);
+
+		if (argc == 4) {
+			args.detailAmplification = std::stof(argv[3]);
+		}
+
+		if (argc == 5) {
+			args.denoiseInputImage = (std::stoi(argv[4]) != 0);
+		}
+
+		if (args.sigma <= 0) {
+			return std::nullopt;
+
+		}
+		return args;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error parsing arguments: " << e.what() << "\n";
+	}
+	
+}
 
